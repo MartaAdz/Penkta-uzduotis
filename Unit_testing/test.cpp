2f9080d384c13c5da447fa306bdcfc3e1716dbba
Marta <adzgausku@gmail.com>
2018-05-24 10:15:02 +0300

    updated test

--- Unit_testing/test.cpp
+++ Unit_testing/test.cpp
@@ -10,21 +10,15 @@
 TEST_CASE( "testuojamas map konteineris" ) {
 
     std::map<std::string, counter> cnt;
-    std::ifstream data ("test.txt");
-    fill_map(cnt, data);
+
+    std::string tmp = "Vilniuje";
+    cnt[tmp]++;
 
     SECTION("Pasikartojimas") {
 
-        std::string tmp = "Vilniuje";
-        REQUIRE(cnt[tmp].get_value() == 2);
+        REQUIRE(cnt[tmp].get_value() == 1);
     }
 
-    SECTION("Eilutes") {
-
-        std::string tmp = "Vilniuje";
-        std::vector<unsigned int> vec {2, 4};
-        REQUIRE(find_line(tmp, data) == vec);
-    }
 }
 
 
---
+++ cmake-build-debug/CMakeFiles/Testas.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Testas.dir/foo.cpp.obj"
+  "CMakeFiles/Testas.dir/Unit_testing/test.cpp.obj"
+  "Testas.pdb"
+  "Testas.exe"
+  "Testas.exe.manifest"
+  "libTestas.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Testas.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
